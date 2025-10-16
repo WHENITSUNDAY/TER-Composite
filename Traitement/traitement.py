5a6b30dcd6567d6efbec57f825049f507530ca1a
WHENITSUNDAY <julien.leger@bordeaux-inp.fr>
2025-10-16 15:29:19 +0200

    Transformation du fichier traitement.py afin qu'il soit utilisé depuis le main, il suffit d'appeler le fichier avec le chemin vers l'image à traiter avec une option show pour afficher avec OpenCv la détection des cercles. Les coordonnées et rayons des cercles sont exportés dans le fichier circles.dat.

--- Traitement/traitement.py
+++ Traitement/traitement.py
@@ -1,27 +1,43 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
+import sys
 
+"""
+Ce code permet de détecter des cercles dans une image de matériau composite avec la transformée de Hough en utilisant la librarie OpenCv.
+Ces cercles correspondent aux fibres du matériau, et leurs paramètres (coordonées du centre/rayon) sont exportées dans un fichier qui sera 
+ensuite utilisé pour la création du maillage avec Gmsh. Ce dernier sera utilisé pour la simulation par éléments finis.
+"""
 
-img = cv.imread('images_test/1.png')
-gray = cv.imread('images_test/1.png', cv.IMREAD_GRAYSCALE)
+if __name__ == "__main__":
 
-mean = np.mean(gray)
-#binary = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-_, binary = cv.threshold(gray, mean - 20, 255, cv.THRESH_BINARY)
+    if len(sys.argv) > 3 or len(sys.argv) < 2:
+        print("Utilisation type : python traitement.py <image_path> [show](optionnel)")
+        sys.exit(1)
 
-cv.imshow('Image binaire', binary)
-cv.waitKey(0)
+    img_path = sys.argv[1]
+    show = True if len(sys.argv) > 2 else False
+    
+    img = cv.imread(img_path)
 
-blur = cv.GaussianBlur(binary, (9, 9), 2)
+    #Prétaitement de l'image (recadrage, conversion en niveaux de gris et floutage)
+    L = max(img.shape)  
+    gray = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
+    blur = cv.GaussianBlur(gray, (9, 9), 2)
 
-circles = cv.HoughCircles(blur, method=cv.HOUGH_GRADIENT, dp=1, minDist=20, param1=100, param2=18, minRadius=25, maxRadius=50)
+    #Détection des cercles avec la transformée de Hough
+    circles = cv.HoughCircles(blur, method=cv.HOUGH_GRADIENT, dp=1, minDist=50, param1=50, param2=18, minRadius=25, maxRadius=50)
 
-if circles is not None:
-    circles = np.uint16(np.around(circles))
-    for i in circles[0, :]:
-        cv.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        cv.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+    print(f"Nombre de cercles détectés : {0 if circles is None else len(circles[0])}")
+    print(f"Ecriture des paramètres (coordonnées du centre et rayon) des cercles dans le fichier circles.dat")
 
-cv.imshow('Detected Circles', img)
-cv.waitKey(0)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        with open('circles.dat', 'w') as f: 
+            for i in circles[0, :]:
+                f.write(f"{i[0]/L} {i[1]/L} {i[2]/L}\n")
+                cv.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
+                cv.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+    if show:
+        cv.imshow('Detected Circles', img)
+        cv.waitKey(0)
