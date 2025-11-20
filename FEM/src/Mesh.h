29f64828216d16991eb7b771e24c08f053068695
WHENITSUNDAY <julien.leger@bordeaux-inp.fr>
2025-11-20 16:50:20 +0100

    Création du header Mesh.h qui contient les classes : Node (noeuds du maillage), Element (Triangles P1 du maillage (repérés par 3 noeuds)), et la classe globale Mesh qui contient tous les Elements et les Nodes. Dans la classe Element, une méthode permet de calculer la matrice de rigidité élémentaire en P1. Pour la classe mesh, elle construit le maillage grâce à un fichier .mesh

---
+++ FEM/src/Mesh.h
@@ -0,0 +1,70 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <vector>
+#include <string>
+#include <Eigen/Dense>
+
+class Node {
+    // Classe représentant un noeud du maillage (coordonnées et numéro)
+    private:
+        int _id;
+        Eigen::Vector2d _coords;
+
+    public:
+        Node(int nodeId, const Eigen::Vector2d& coords);
+        int getId() const { return _id; }
+        Eigen::Vector2d getCoords() const { return _coords; }
+        void setCoords(const Eigen::Vector2d& c) { _coords = c; }
+};
+
+class Element {
+    // Classe représentant un élément (triangle P1) du maillage avec ses noeuds et le matériau associé.
+    private:
+        int _id;
+        // Les noeuds sont référencés par leurs indices, ce qui évite de dupliquer des objets Node inutilement,
+        // Sachant que les elements partagent des noeuds entre eux.
+        std::vector<int> _nodeIds; 
+        
+        class Material* _mat;
+        double _area;
+        Eigen::Matrix<double, 6, 6> _Ke; // Matrice de rigidité élémentaire propre à l'élément P1     
+
+    public:
+        Element(int elemId, const std::vector<int>& nodeIds, Material* mat);    
+        
+        int getId() const { return _id; }
+        double getArea() const { return _area; }
+        Material* getMaterial() const { return _mat; }
+        const std::vector<int>& getNodeIds() const { return _nodeIds; }
+
+        void setArea(double A) { _area = A; }
+        void computeKe();
+};
+
+class Mesh {
+    // Classe globale représentant le maillage en utilisant des listes d'objets Node et Element.
+    // Permet de charger un maillage depuis un fichier Gmsh et de le manipuler dans le programme.
+    private:
+        std::vector<Node> _nodes; 
+        std::vector<Element> _elements;
+
+    public:
+        Mesh();
+
+        void addNode(const Node& node);
+        void addElement(const Element& element);
+
+        const std::vector<Node>& getNodes() const { return _nodes; }
+        const std::vector<Element>& getElements() const { return _elements; }  
+
+        Node& getNode(int id);
+        Element& getElement(int id);
+
+        int getNbNodes() const { return _nodes.size(); }
+        int getNbElements() const { return _elements.size(); }
+        
+        void loadFromGmsh(const std::string& filename);
+};
+
+#endif
